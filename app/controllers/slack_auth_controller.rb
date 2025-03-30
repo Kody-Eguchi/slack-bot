d6bd3f69fe79da4710fb6823d61233d0ae55523f
Koji Eguchi <koji.eguchi@hotmail.com>
2025-03-30 18:03:26 -0400

    Update bot scope permission

--- app/controllers/slack_auth_controller.rb
+++ app/controllers/slack_auth_controller.rb
@@ -7,7 +7,7 @@ class SlackAuthController < ApplicationController
   def install
     slack_auth_url = "https://slack.com/oauth/v2/authorize?" +
                      "client_id=#{ENV['SLACK_CLIENT_ID']}&" +
-                     "scope=commands,chat:write&" +
+                     "scope=chat:write,channels:manage,channels:read,commands,groups:write,im:write,mpim:write&" +
                      "redirect_uri=#{ENV['APP_HOST']}/slack/oauth/callback"
     redirect_to slack_auth_url, allow_other_host: true
   end
