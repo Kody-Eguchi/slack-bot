6b4b9c42980c66bc85eee17b4c918c3a242f5245
Koji Eguchi <koji.eguchi@hotmail.com>
2025-03-31 11:56:31 -0400

    Fix team_id error

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -5,43 +5,61 @@ class SlackService
     @slack_client ||= Slack::Web::Client.new(token: ENV['SLACK_BOT_TOKEN'])
   end
 
+  def self.slack_client(team_id)
+    token = Rails.cache.read("slack_#{team_id}_token")
+  
+    if token.nil?
+      Rails.logger.error("❌ No stored access token for team #{team_id}")
+      return nil
+    end
+  
+    Slack::Web::Client.new(token: token)
+  end
+
   def self.declare_incident(parts, slack_event)
     title, description, severity = new.handle_params(parts)
     user_id, user_name = slack_event.values_at('user_id', 'user_name')
+    team_id = slack_event['team_id']
     
     Rails.logger.info "User Info: User ID: #{user_id}, User Name: #{user_name}"
     Rails.logger.info "Slack Event: #{slack_event.inspect}"
 
+    slack_client_instance = slack_client(team_id)
+    return unless slack_client_instance
+
+    auth_response = slack_client_instance.auth_test
+    if auth_response["ok"]
+      bot_team_id = auth_response["team_id"]
+      Rails.logger.info("✅ Bot authenticated in team: #{bot_team_id}")
+
+      if bot_team_id != team_id
+        Rails.logger.error("🚨 Mismatch! Expected team #{team_id}, but bot is in #{bot_team_id}")
+        return
+      end
+    else
+      Rails.logger.error("❌ Bot authentication failed: #{auth_response['error']}")
+      return
+    end
+
+
+    # Rails.logger.info("Retrieved token for team #{team_id} from Redis: #{stored_token}")
     # Create an incident in database
     incident = Incident.create!(title: title, description: description, severity: severity, creator: user_name)
     
     # Create a new Slack channel for the incident
-    response = slack_client.conversations_create(name: title)
-
+    # response = slack_client.conversations_create(name: title)
+    response = slack_client_instance.conversations_create(name: title)
     Rails.logger.info "Slack API Response: #{response.inspect}"
+  
+    return unless response['ok']
 
     # Check bot's membership in the newly created channel
     slack_channel_id = response['channel']['id']
     channel_info = slack_client.conversations_info(channel: slack_channel_id)
 
-    auth_response = slack_client.auth_test
-    bot_user_id = auth_response['user_id']
     
    
-      invite_response = slack_client.conversations_invite(channel: slack_channel_id, users: bot_user_id)
-      Rails.logger.info "Bot Invite Response: #{invite_response.inspect}"
     
-
-    auth_response = slack_client.auth_test
-    if auth_response["ok"]
-      Rails.logger.info("🚨🚨🚨Authenticated Slack user: #{auth_response['user']}")
-    else
-      Rails.logger.error("🚨🚨🚨Slack authentication failed: #{auth_response['error']}")
-    end
-
-    channels_response = slack_client.conversations_list
-    channels = channels_response['channels']
-    Rails.logger.info("List of channels in the workspace: #{channels.map { |channel| channel['name'] }.join(', ')}")
   
     # Get the Slack channel ID from the response
     slack_channel_id = response['channel']['id']
@@ -49,11 +67,14 @@ class SlackService
 
     # inviting the user to a new channel
     Rails.logger.info "User ID before inviting: #{user_id.inspect}"
-    invite_response = slack_client.conversations_invite(channel: slack_channel_id, users: user_id)
+    # invite_response = slack_client.conversations_invite(channel: slack_channel_id, users: user_id)
+    invite_response = slack_client_instance.conversations_invite(channel: slack_channel_id, users: user_id)
     Rails.logger.info "Slack Invite Response: #{invite_response.inspect}"
 
     # Post a summary of the incident in the new channel
-    slack_client.chat_postMessage(channel: slack_channel_id, 
+    # slack_client.chat_postMessage(
+    slack_client_instance.chat_postMessage(
+    channel: slack_channel_id, 
     text: "🚨 *Incident Declared!* 🚨\n" \
     "*Title:* #{title}\n" \
     "*Description:* #{description || 'No description provided'}\n" \
