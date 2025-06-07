a8a0539250ec2136e896062f05a9d4c9b6ecc1a6
Koji Eguchi <koji.eguchi@hotmail.com>
2025-06-06 20:23:26 -0400

    Create sort_option method and delete inline logics

--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -6,4 +6,10 @@ module IncidentsHelper
     else 'text-gray-600'
     end
   end
+
+  def sort_option(label, sort_field, direction)
+    selected = (params[:sort]== sort_field && params[:direction] == direction) ? 'selected' : ''
+    value = incidents_path(sort: sort_field, direction: direction)
+    "<option value='#{value}' #{selected}>#{label}</option>".html_safe
+  end
 end
