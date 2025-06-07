ffe7d124228c4b29e0f515745a5306e477e29d72
Koji Eguchi <koji.eguchi@hotmail.com>
2025-06-07 18:15:48 -0400

    Validate data in incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,2 +1,5 @@
 class Incident < ApplicationRecord
+  validates :title, presence: true
+  validates :severity, inclusion: { in: %w[sev0 sev1 sev2 sev3], allow_blank: true }
+  validates :status, inclusion: { in: %w[open resolved], message: "%{value} is not a valid status" }
 end
