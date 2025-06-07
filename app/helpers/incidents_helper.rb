b8df972a32e28c62623f3ce90fd6bb034e78c967
Koji Eguchi <koji.eguchi@hotmail.com>
2025-06-06 20:11:49 -0400

    Create status_css_class method and delete inline logic

--- app/helpers/incidents_helper.rb
+++ app/helpers/incidents_helper.rb
@@ -1,2 +1,9 @@
 module IncidentsHelper
+  def status_css_class(status)
+    case status
+    when 'open' then 'text-red-600'
+    when 'resolved' then 'text-green-600'
+    else 'text-gray-600'
+    end
+  end
 end
