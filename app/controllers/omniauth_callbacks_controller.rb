a542d6fe32a17bce0cf6f3b9a7b2c1a16d33c342
Koji Eguchi <koji.eguchi@hotmail.com>
2025-07-02 21:42:23 -0400

    Add omniauth controller

---
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -0,0 +1,17 @@
+class OmniauthCallbacksController < ApplicationController
+  def slack
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash[:notice] = "Successfully authenticated from Slack account." if is_navigational_format?
+    else
+      session["devise.slack_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url, alert: "Slack authentication failed."
+    end
+  end
+
+  def failure
+    redirect_to root_path
+  end
+end
