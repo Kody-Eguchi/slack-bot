8de5fb3e080de4537eb6881249e22aca7c67ee22
Koji Eguchi <koji.eguchi@hotmail.com>
2025-07-03 20:24:12 -0400

    Add sign in page

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -4,7 +4,7 @@ class OmniauthCallbacksController < ApplicationController
 
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
-      set_flash[:notice] = "Successfully authenticated from Slack account." if is_navigational_format?
+      set_flash_message(:notice, :success, kind: "Slack") if is_navigational_format?
     else
       session["devise.slack_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url, alert: "Slack authentication failed."
