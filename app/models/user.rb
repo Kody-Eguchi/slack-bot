4d6cdf1465c734868ae54824984e88804e376e84
Koji Eguchi <koji.eguchi@hotmail.com>
2025-07-06 17:28:11 -0400

    Remove omniauth related files and lines

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,
-         :omniauthable, omniauth_providers: [:slack]
-
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email || "#{auth.uid}@slack.local"
-      user.password = Devise.friendly_token[0, 20]
-      user.slack_user_name = auth.info.name
-      user.slack_user_id = auth.uid
-    end
-  end
+         :recoverable, :rememberable, :validatable
 end
